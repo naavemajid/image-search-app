95eb2af371c272e2b5be258ecdaf6646fa36a8d5
Majid Naave <naavemajid2001@gmail.com>
2024-01-11 14:45:06 -0800

    JavaScript was added

---
+++ script.js
@@ -0,0 +1,56 @@
+const accessKey = "4YiO3pDJBtpmRNZ87CZjoHtBckgOhhhpgwx-tTRYgqA";
+
+const formEl = document.querySelector('form');
+const inputEl = document.getElementById('searcg-input');
+const searchResults = document.querySelector('.search-results');
+const showMore = document.getElementById('show-more-button');
+
+let inputData = "";
+let page = 1;
+
+
+async function searchImages(){
+    inputData = inputEl.ariaValueMax;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if(page === 1){
+        searchResults.innerHTML = "";
+    }
+    results.map((result)=>{
+        const imagewrapper = document.createElement('div');
+        imagewrapper.classList.add('search-result');
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = '_blank';
+        imageLink.textContent = result.alt_description;
+
+        imagewrapper.appendChild(image);
+        imagewrapper.appendChild(imageLink);
+        imagewrapper.appendChild(imagewrapper);
+    })
+    page++;
+
+    if(page > 1){
+        showMore.style.display = 'block';
+    }
+}
+
+formEl.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMore.addEventListener('click', ()=>{
+    searchImages();
+})
+
+
